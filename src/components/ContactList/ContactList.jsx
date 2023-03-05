11484be173ef968c9b383b25f9e5e67fb416c5a6
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2023-03-05 22:21:49 +0200

    add table for contacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -20,24 +20,62 @@ export default function ContactList() {
   };
 
   const contactsToRender = getFilteredContacts();
-    const elements = (
-      
-    <ul className={styles.list}>
-      {contactsToRender.map(item => (
-        <li className={styles.item} key={item.id}>
-          <p>
-            {item.name}: {item.number}
-          </p>
-          <button
-            type="button"
-            className={styles.button}
-            onClick={() => dispatch(removeContact(item.id))}
-          >
-            delete
-          </button>
-        </li>
-      ))}
-    </ul>
+ 
+  //   const elements = (
+  //     <ul className={styles.list}>
+  //       {contactsToRender.map(item => (
+  //         <li className={styles.item} key={item.id}>
+  //           <p>
+  //             {item.name}: {item.number}
+  //           </p>
+  //           <button
+  //             type="button"
+  //             className={styles.button}
+  //             onClick={() => dispatch(removeContact(item.id))}
+  //           >
+  //             delete
+  //           </button>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+
+  return (
+    <>
+      {' '}
+      {contactsToRender.length > 0 ? (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.thead}>№</th>
+              <th className={styles.thead}>Name</th>
+              <th className={styles.thead}>Number</th>
+              <th className={styles.thead}>Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {contactsToRender.map((item, index) => (
+              
+              <tr key={item.id}>
+                <td className={styles.data}>{index + 1}</td>
+                <td className={styles.data}>{item.name}</td>
+                <td className={styles.data}>{item.number}</td>
+                <td className={styles.data}>
+                  <button
+                    type="button"
+                    className={styles.button}
+                    onClick={() => dispatch(removeContact(item.id))}
+                  >
+                    delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className={styles.empty}>No contacts</p>
+      )}
+    </>
   );
-  return <>{contactsToRender.length > 0 ? elements : <p className={styles.empty}>No contacts</p>}</>;
 }
